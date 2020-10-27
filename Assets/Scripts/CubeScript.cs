25a1517afd7b388466aa2e82adf1e072087f1244
Ekaterina Kazakevich <ekaterinaka@playtika.com>
2020-10-27 13:11:33 +0300

    Project init, simple animator

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    private Animator _animator;
+    
+    private const string Trigger = "trigger";
+    private static readonly int TriggerHash = Animator.StringToHash(Trigger);
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(TriggerHash);
+        }
+    }
+}
---
+++ Assets/Scripts/RoboBoooooi.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RoboBoooooi : MonoBehaviour
+{
+    private Animator _animator;
+    
+    private const string IsMovingStr = "IsMoving";
+    private static readonly int IsMovingHash = Animator.StringToHash(IsMovingStr);
+    
+    private const string IsJumpingStr = "IsJumping";
+    private static readonly int IsJumpingHash = Animator.StringToHash(IsJumpingStr);
+    
+    private const string IsCrouchingStr = "IsCrouching";
+    private static readonly int IsCrouchingHash = Animator.StringToHash(IsCrouchingStr);
+    
+    private bool _isMoving;
+    private bool _isJumping;
+    private bool _isCrouching;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private bool IsMoving
+    {
+        get => _isMoving;
+        set
+        {
+            _isMoving = value;
+            _animator.SetBool(IsMovingHash, _isMoving);
+        }
+    }
+
+    private bool IsJumping
+    {
+        get => _isJumping;
+        set
+        {
+            _isJumping = value;
+            _animator.SetBool(IsJumpingHash, _isJumping);
+        }
+    }
+
+    private bool IsCrouching
+    {
+        get => _isCrouching;
+        set
+        {
+            _isCrouching = value;
+            _animator.SetBool(IsCrouchingHash, _isCrouching);
+        }
+    }
+    void Update()
+    {
+        IsMoving = Input.GetAxis("Horizontal")>0;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            IsJumping = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            IsJumping = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            IsCrouching = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            IsCrouching = false;
+        }
+    }
+}
